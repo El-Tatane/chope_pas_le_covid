4cbeed4d690f87dbe8bead9d1e6ae7cbb119e031
El-Tatane <antoine@boudeville.fr>
2020-05-14 13:11:16 +0200

    end

--- etl/graphe.py
+++ etl/graphe.py
@@ -1,24 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[168]:
-
-
-#Import bibli
 import pandas as pd
 pd.set_option('display.max_columns', None)
 pd.set_option('max_colwidth',800)
 
 
+# Cleaning and get_Data
 
-
-
-#Modification des csv
-
-
-#Import csv
-
-def transform(df):
+def Cleaning_begining(df):
     df.index=df["Country/Region"]
     df=df.drop(columns=["Country/Region",'Lat','Long','Province/State'])
     df=df.groupby("Country/Region").sum()
@@ -26,53 +13,57 @@ def transform(df):
 
 
 def get_confirm():
-    confirm=pd.read_csv('/home/covid/dataset/global/time_series_covid19_confirmed_global.csv', sep = ',')
-    confirm=transform(confirm)
+    confirm=pd.read_csv('../../dataset/global/time_series_covid19_confirmed_global.csv', sep = ',')
+    confirm=Cleaning_begining(confirm)
     return confirm
 
 
 def get_deaths():
-    deaths =pd.read_csv('/home/covid/dataset/global/time_series_covid19_deaths_global.csv', sep = ',')
-    deaths=transform(deaths)
+    deaths =pd.read_csv('../../dataset/global/time_series_covid19_deaths_global.csv', sep = ',')
+    deaths=Cleaning_begining(deaths)
     return deaths
 
 
 def get_recover():
-    recover=pd.read_csv('/home/covid/dataset/global/time_series_covid19_recovered_global.csv', sep = ',')
-    recover=transform(recover)
+    recover=pd.read_csv('../../dataset/global/time_series_covid19_recovered_global.csv', sep = ',')
+    recover=Cleaning_begining(recover)
     return recover
 
-def get_total():
-    total=pd.read_csv('/home/covid/dataset/global/WPP2019_TotalPopulationBySex.csv', sep = ',')
+def get_csv_count_population():
+    total=pd.read_csv('../../dataset/global/WPP2019_TotalPopulationBySex.csv', sep = ',')
     total=total[total["Time"]==2019]
     return total
 
+def get_unification_Recorvery_Confirm_Death(confirm,recover,deaths):
+    confirm["name"]="confirm"
+    recover["name"]="recover"
+    deaths["name"]="deaths"
+    return pd.concat([confirm, recover,deaths])
 
+# Methode filtre graphe
 
-# ### Methode
-
-# In[171]:
-
-
-def conf_by_country(df,country):
+def filter_by_country(df,country):
     return df.loc[country]
+
 def total_by_country(df,country):
     return int(df.loc[(df["Location"]==country)&(df["Time"]==2019),"PopTotal"].values[0] * 1000)
-def Graphe(confirm,recover,deaths,country,total):
-    confirm_country=conf_by_country(confirm,country)
-    confirm_country.plot.line()
 
+def get_data(all_data,country):
+    return filter_by_country(all_data,country)
 
+def get_data(country):
+    return filter_by_country(all_data,country), total_by_country(csv_count_population,country)
 
-    recover_country=conf_by_country(recover,country)
-    recover_country.plot.line()
+def plot_result( DataFrame_predict,DataFrame_reel=None ):
+    name_type=DataFrame_predict["name"]
+    DataFrame_predict=DataFrame_predict.drop(columns="name")
+    DataFrame_predict=DataFrame_predict.T
+    DataFrame_predict.columns=name_type
+    DataFrame_predict.plot.line(subplots=True)
+    if DataFrame_reel  is not None:
 
-    country_death=conf_by_country(deaths,country)
-
-    potential_contamination = total_by_country(total,country) - (country_death + recover_country)
-    # potential_contamination = 800000 - (country_death + recover_country)
-    potential_contamination.plot.line(logy=True)
-    #mal.plot.line(logy=True)
-
-
-# In[ ]:
+        name_type=DataFrame_reel["name"]
+        DataFrame_reel=DataFrame_reel.drop(columns="name")
+        DataFrame_reel=DataFrame_reel.T
+        DataFrame_reel.columns=name_type
+        DataFrame_reel.plot.line(subplots=True)
