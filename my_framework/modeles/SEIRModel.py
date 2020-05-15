dfaf39fb319556ea90b3b49fbedb0e28cfeb94f1
El-Tatane <antoine@boudeville.fr>
2020-05-15 17:09:15 +0200

    end

--- my_framework/modeles/SEIRModel.py
+++ my_framework/modeles/SEIRModel.py
@@ -26,12 +26,34 @@ class SEIRModel():
         SEIR_0 = S_0, E_0, I_0, R_0
         return SEIR_0
 
-    def fit(self, y, N, E_0=1):
+    def fit(self, N, I, R=None, E_0=1):
+            if R is None:
+                return self.fit_I(I, N, E_0)
+            else:
+                return self.fit_IR(I, R, N, E_0)
+
+    def fit_I(self, y, N, E_0):
+        if y[0] == 0 : raise ValueError("Need an infected at the beginning")
         t = np.linspace(0, len(y), len(y))
 
         SEIR_0 = self.get_SEIR_0(N, y[0], E_0)
 
         f = lambda t, beta, gamma, delta : (odeint(self.deriv, SEIR_0, t, args=(N, beta, gamma, delta)).T)[2]
+
+        params, covariance = curve_fit(f, t, y)
+        self.params["beta"] = params[0]
+        self.params["gamma"] = params[1]
+        self.params["delta"] = params[2]
+        return params, covariance
+
+    def fit_IR(self, I, R, N, E_0):
+        if I[0] == 0 : raise ValueError("Need an infected at the beginning")
+        t = np.linspace(0, len(I)*2, len(I)* 2)
+        y = np.hstack((I, R))
+
+        SIER_0 = self.get_SEIR_0(N, I[0], E_0)
+
+        f = lambda t, beta, gamma, delta : np.hstack((odeint(self.deriv, SIER_0, t, args=(N, beta, gamma, delta)).T)[1:2])
 
         params, covariance = curve_fit(f, t, y)
         self.params["beta"] = params[0]
