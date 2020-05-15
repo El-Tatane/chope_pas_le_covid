79872027e676bc3f26a837975c947f01e67680ae
El-Tatane <antoine@boudeville.fr>
2020-05-15 12:00:43 +0200

    save

---
+++ my_framework/modeles/SEIRModel.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+from scipy.integrate import solve_ivp, odeint
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+class SEIRModel():
+    """
+    Info SEIR model
+
+    S = those susceptible but not yet infected with the disease
+    E = individuals that have contracted the disease but are not yet infectious
+    I = the number of infectious individuals
+    R = those individuals who have recovered from the disease and now have immunity to it
+
+    """
+
+    def __init__(self, beta=0.3, gamma=0.1, delta=0.3):
+        self.params = {}
+        self.params["beta"] = beta
+        self.params["gamma"] = gamma
+        self.params["delta"] = delta
+
+    def get_SEIR_0(self, I_0, N):
+        R_0, I_0 = 0, 0
+        S_0 = N - E_0 - I_0 - R_0
+        SEIR_0 = S_0, E_0, I_0, R_0
+        return SEIR_0
+
+    def fit(self, y, N, E_0):
+        t = np.linspace(0, len(y), len(y))
+
+        SEIR_0 = self.get_SEIR_0(E_0, N)
+
+        f = lambda t, beta, gamma, delta : (odeint(self.deriv, SEIR_0, t, args=(N, beta, gamma, delta)).T)[2]
+
+        params, covariance = curve_fit(f, t, y)
+        self.params["beta"] = params[0]
+        self.params["gamma"] = params[1]
+        self.params["delta"] = params[2]
+        return params, covariance
+
+    def deriv(self, y, t, N, beta, gamma, delta):
+        S, E, I, R = y
+        dSdt = -beta * S * I / N
+        dEdt = beta * S * I / N - delta * E
+        dIdt = delta * E - gamma * I
+        dRdt = gamma * I
+        return dSdt, dEdt, dIdt, dRdt
+
+    def predict(self, t, N, E_0):
+        SEIR_0 = self.get_SEIR_0(E_0, N)
+        res = odeint(self.deriv, SEIR_0, t, args=(N, self.params["beta"], self.params["gamma"], self.params["delta"]))
+        return res.T
--- my_framework/plot.py
+++ my_framework/plot.py
@@ -40,3 +40,68 @@ def plot_triple_sir(list_t, list_predict_S, list_predict_I, list_predict_R, list
                 ax.spines[spine].set_visible(False)
             fig.suptitle(tilte)
             plt.show()
+
+
+def plotseird(t, S, E, I, R, D=None, L=None, R0=None, Alpha=None, CFR=None):
+    f, ax = plt.subplots(1,1,figsize=(10,4))
+    ax.plot(t, S, 'b', alpha=0.7, linewidth=2, label='Susceptible')
+    ax.plot(t, E, 'y', alpha=0.7, linewidth=2, label='Exposed')
+    ax.plot(t, I, 'r', alpha=0.7, linewidth=2, label='Infected')
+    ax.plot(t, R, 'g', alpha=0.7, linewidth=2, label='Recovered')
+    if D is not None:
+        ax.plot(t, D, 'k', alpha=0.7, linewidth=2, label='Dead')
+        ax.plot(t, S+E+I+R+D, 'c--', alpha=0.7, linewidth=2, label='Total')
+    else:
+        ax.plot(t, S+E+I+R, 'c--', alpha=0.7, linewidth=2, label='Total')
+
+    ax.set_xlabel('Time (days)')
+
+    ax.yaxis.set_tick_params(length=0)
+    ax.xaxis.set_tick_params(length=0)
+    ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+    legend = ax.legend(borderpad=2.0)
+    legend.get_frame().set_alpha(0.5)
+    for spine in ('top', 'right', 'bottom', 'left'):
+        ax.spines[spine].set_visible(False)
+    if L is not None:
+        plt.title("Lockdown after {} days".format(L))
+    plt.show();
+
+    if R0 is not None or CFR is not None:
+        f = plt.figure(figsize=(12,4))
+
+    if R0 is not None:
+        # sp1
+        ax1 = f.add_subplot(121)
+        ax1.plot(t, R0, 'b--', alpha=0.7, linewidth=2, label='R_0')
+
+        ax1.set_xlabel('Time (days)')
+        ax1.title.set_text('R_0 over time')
+        # ax.set_ylabel('Number (1000s)')
+        # ax.set_ylim(0,1.2)
+        ax1.yaxis.set_tick_params(length=0)
+        ax1.xaxis.set_tick_params(length=0)
+        ax1.grid(b=True, which='major', c='w', lw=2, ls='-')
+        legend = ax1.legend()
+        legend.get_frame().set_alpha(0.5)
+        for spine in ('top', 'right', 'bottom', 'left'):
+            ax.spines[spine].set_visible(False)
+
+    if Alpha is not None:
+        # sp2
+        ax2 = f.add_subplot(122)
+        ax2.plot(t, Alpha, 'r--', alpha=0.7, linewidth=2, label='alpha')
+
+        ax2.set_xlabel('Time (days)')
+        ax2.title.set_text('fatality rate over time')
+        # ax.set_ylabel('Number (1000s)')
+        # ax.set_ylim(0,1.2)
+        ax2.yaxis.set_tick_params(length=0)
+        ax2.xaxis.set_tick_params(length=0)
+        ax2.grid(b=True, which='major', c='w', lw=2, ls='-')
+        legend = ax2.legend()
+        legend.get_frame().set_alpha(0.5)
+        for spine in ('top', 'right', 'bottom', 'left'):
+            ax.spines[spine].set_visible(False)
+
+    plt.show();
